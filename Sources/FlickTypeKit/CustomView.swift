497b5a7a3f6685629604fecb06364a335ce37587
llsc12 <llsc12.dev@gmail.com>
2022-08-07 20:13:14 +0100

    e

--- Sources/FlickTypeKit/CustomView.swift
+++ Sources/FlickTypeKit/CustomView.swift
@@ -11,13 +11,13 @@ import SwiftUI
 @available(watchOS 8.0, *)
 public struct FlickTypeTextField: View {
     
-    private var flicktype: Bool
-    private var title: LocalizedStringKey
+    @State private var flicktype: Bool
+    @State private var title: LocalizedStringKey
     @Binding private var text: String
     
-    public init(flicktype: Bool, title: LocalizedStringKey, text: Binding<String>) {
-        self.flicktype = flicktype
-        self.title = title
+    public init(flicktype: State<Bool>, title: State<LocalizedStringKey>, text: Binding<String>) {
+        self._flicktype = flicktype
+        self._title = title
         self._text = text
     }
     
